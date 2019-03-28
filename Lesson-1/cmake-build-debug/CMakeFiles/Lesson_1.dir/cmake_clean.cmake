c39e558fdc1125fa607a91c20b605727c0fcb893
species84720 <Species84720@users.noreply.github.com>
2019-03-28 10:17:03 +0100

    moving things around
    Integrating lesson-1 git to current git

---
+++ Lesson-1/cmake-build-debug/CMakeFiles/Lesson_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lesson_1.dir/main.c.o"
+  "Lesson_1.pdb"
+  "Lesson_1.exe"
+  "libLesson_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lesson_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
